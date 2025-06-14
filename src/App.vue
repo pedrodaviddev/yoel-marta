4c3cd111296395e774c8f97407a14d2c47efae3c
pedrodaviddev <pedrodaviddev@gmail.com>
2025-06-14 22:21:40 +0200

    Added schedule

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,15 @@ import './assets/main.css';
 import Countdown from './components/Countdown.vue';
 import PhoneButton from "@/components/PhoneButton.vue";
 import meme from './assets/meme.mp3'
-import {ref} from "vue";
+import cake from './assets/cake.svg'
+import disco from './assets/disco.svg'
+import rings from './assets/rings.svg'
+import cocktail from './assets/cocktail.svg'
+import { ref } from "vue";
 
 const audio = ref(new Audio(meme))
 const hasPlayed = ref(!window.location.search.includes('ivanesgay'));
-const layerVisible = ref(true);
+const layerVisible = ref(!true);
 window.addEventListener('scroll', () => {
   if (!hasPlayed.value) {
     audio.value.play();
@@ -22,7 +26,8 @@ window.addEventListener('scroll', () => {
   <Transition name="fade" mode="out-in">
     <div class="w-100 h-100 bg-black" v-if="layerVisible">
       <div class="position-relative">
-        <v-img class="w-full ma-0 p-0 opacity-40 font-beautifully " position="top" style="filter: blur(4px)" cover height="100vh" :src="bg"/>
+        <v-img class="w-full ma-0 p-0 opacity-40 font-beautifully " position="top" style="filter: blur(4px)" cover
+               height="100vh" :src="bg"/>
         <v-btn class="position-absolute" variant="outlined" color="white"
                style="top:50%; left: 50%; transform: translate(-50%,-50%)"
                @click="layerVisible = false">Descubre
@@ -89,10 +94,20 @@ window.addEventListener('scroll', () => {
         <div class="font-parfumerie text-h2" style="color: #504d4d">
           Programa
         </div>
-        <div class="font-newstandard">
-          (Próximamente daremos detalles)
+        <div class="d-flex flex-column ga-6  icon-images font-cmu">
+          <div class="d-flex w-100 ga-4 align-center justify-start text-grey-darken-2">
+            <v-img :aspect-ratio="1" contain height="28px" width="28px" :src="rings"/>
+            <span>19:30 Ceremonia</span></div>
+          <div class="d-flex w-100 ga-4 align-center">
+            <v-img :aspect-ratio="1" contain height="28px" width="28px" :src="cocktail"/>
+            <span>20:15 Cóctel</span></div>
+          <div class="d-flex  w-100 ga-4 align-center">
+            <v-img :aspect-ratio="1" contain height="28px" width="28px" :src="cake"/>
+            <span>21:00 Banquete</span></div>
+          <div class="d-flex w-100 ga-4 align-center">
+            <v-img :aspect-ratio="1" contain height="28px" width="28px" :src="disco"/>
+            <span>23:30 Baile</span></div>
         </div>
-        <v-progress-circular color="white" indeterminate></v-progress-circular>
 
         <v-divider :thickness="2"/>
         <div class="px-4 text-center font-newstandard" style="color: #504d4d">
@@ -109,12 +124,13 @@ window.addEventListener('scroll', () => {
         <div class="font-beautifully text-h4 text-no-wrap font-weight-bold">
           ¿Quieres confirmar tu asistencia?
         </div>
-        <div class="d-flex ga-2 mb-10" style="color: #504d4d">
+        <div class="d-flex ga-2 mb-8" style="color: #504d4d">
           <PhoneButton :number="699480616" name="Y"/>
           <div class="mt-2">-</div>
           <PhoneButton :number="684098105" name="M" :icons-on-right="true"/>
         </div>
-        <v-img class="mb-3 mt-16" :src="ym" width="30vw"/>
+        <span class="my-8 text-caption">Fecha límite para confirmar asistencia 15 Junio 2025</span>
+        <v-img class="mb-3 mt-8" :src="ym" width="30vw"/>
       </main>
     </div>
   </Transition>
@@ -135,6 +151,7 @@ main {
 .title-18 {
   font-size: 18vw;
 }
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.4s ease;
